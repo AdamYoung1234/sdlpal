52edf91153779b8adc39c4544da5bf0b63abc985
Chen Yang <adamyoung1234@outlook.com>
2023-07-02 20:33:46 -0500

    fix: switch joycon

--- switch/pal_utils.c
+++ switch/pal_utils.c
@@ -21,20 +21,6 @@
 
 #include "main.h"
 
-/*
-// Switch buttons
-#define JOY_A     0
-#define JOY_B     1
-#define JOY_X     2
-#define JOY_Y     3
-#define JOY_PLUS  10
-#define JOY_MINUS 11
-#define JOY_LEFT  12
-#define JOY_UP    13
-#define JOY_RIGHT 14
-#define JOY_DOWN  15
-*/
-
 static int input_event_filter(const SDL_Event *lpEvent, volatile PALINPUTSTATE *state)
 {
 	if (lpEvent->type == SDL_JOYBUTTONDOWN)
